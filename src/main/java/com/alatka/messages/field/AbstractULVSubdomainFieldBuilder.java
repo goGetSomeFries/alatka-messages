7aeda27475d83a4195a8c1ba93ca3c7ee92403ba
liuyibing123456 <liuyibing123456@126.com>
2024-03-31 22:39:29 +0800

    init

---
+++ src/main/java/com/alatka/messages/field/AbstractULVSubdomainFieldBuilder.java
@@ -0,0 +1,66 @@
+package com.alatka.messages.field;
+
+import com.alatka.messages.context.FieldDefinition;
+import com.alatka.messages.context.MessageDefinition;
+import com.alatka.messages.holder.UsageSubdomain;
+import com.alatka.messages.message.MessageBuilder;
+import com.alatka.messages.util.BytesUtil;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 子域类型报文域解析器
+ *
+ * @author ybliu
+ * @see AbstractFieldBuilder
+ * @see FieldBuilder
+ */
+public abstract class AbstractULVSubdomainFieldBuilder extends SubdomainFieldBuilder<UsageSubdomain<Object>> {
+
+    @Override
+    protected byte[] pack(UsageSubdomain<Object> value, FieldDefinition fieldDefinition, Map<String, MessageDefinition> usageMap) {
+        Map<String, Object> holder = value.getHolder();
+        return holder.entrySet().stream()
+                .map(entry -> {
+                    byte[] usageBytes = this.usageId(entry.getKey());
+                    byte[] valueBytes = MessageBuilder.init(usageMap.get(entry.getKey())).pack(entry.getValue());
+                    byte[] lenBytes = this.usageLen(valueBytes.length);
+                    return BytesUtil.concat(usageBytes, lenBytes, valueBytes);
+                }).reduce(new byte[0], BytesUtil::concat);
+    }
+
+    @Override
+    protected UsageSubdomain<Object> unpack(byte[] bytes, FieldDefinition fieldDefinition, Map<String, MessageDefinition> usageMap) {
+        UsageSubdomain<Object> usageSubdomain = new UsageSubdomain<>();
+        AtomicInteger counter = new AtomicInteger(0);
+        while (counter.get() < bytes.length) {
+            byte[] usageIdBytes = Arrays.copyOfRange(bytes, counter.get(), counter.addAndGet(this.usageIdLength()));
+            byte[] usageLenBytes = Arrays.copyOfRange(bytes, counter.get(), counter.addAndGet(this.usageLenLength()));
+            byte[] usageValueBytes = Arrays.copyOfRange(bytes, counter.get(), counter.addAndGet(this.usageLen(usageLenBytes)));
+
+            String usageId = this.usageId(usageIdBytes);
+
+            super.validate(fieldDefinition, usageId);
+
+            MessageDefinition definition = usageMap.get(usageId);
+            Object value = MessageBuilder.init(definition).unpack(usageValueBytes);
+            usageSubdomain.put(usageId, value);
+        }
+        return usageSubdomain;
+    }
+
+    protected abstract int usageIdLength();
+
+    protected abstract int usageLenLength();
+
+    protected abstract String usageId(byte[] bytes);
+
+    protected abstract int usageLen(byte[] bytes);
+
+    protected abstract byte[] usageId(String id);
+
+    protected abstract byte[] usageLen(int length);
+
+}
